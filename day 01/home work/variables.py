a4b793a4c83d6e67fbe5333a2829de991f1f213b
2pac-icecyb <gela6ix9ine@gmail.com>
2025-07-18 19:19:37 +0400

    commit

---
+++ day 01/home work/variables.py
@@ -0,0 +1,15 @@
+first_name = "Gela"
+last_name = "mchedlishvili"
+height = 1.78
+gender = "male"
+
+print(first_name)
+print(last_name)
+print(height)
+print(gender)
+
+name = "tata"
+name2 = "gela"
+
+print(name)
+print(name2)
