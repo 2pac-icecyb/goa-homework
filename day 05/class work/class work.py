3de9a9c99115c626f15a22aa59769ccb319afbae
2pac-icecyb <gela6ix9ine@gmail.com>
2025-08-07 22:31:46 +0400

    commit

---
+++ day 05/class work/class work.py
@@ -0,0 +1,21 @@
+# debuging ის დანიშნულებაა რომ იპოვნოს ბაგები და დაფიქსოს
+
+# case sensetive ნიშნავს დიდ და პატარა ასოების გარჩევას
+
+#2_level ცვლადი იწყება ციფრით
+#LEVel ცვლადი არის სხვადასხვა ასოებით
+#class_work()=random ცვლადში არის სიმბოელები
+
+my_name="gela"
+my_age="14"
+my_level="5"
+favorite_number="7"
+
+
+num1=5
+num2=4
+print(num1+num2)
+print(num1/num2)
+print(num1//num2)
+print(num1-num2)
+print(num1*num2)
---
+++ day 05/home work/homework.py
@@ -0,0 +1,16 @@
+name1="luka" #ცვლადის დაწყება ციფრით არ შეიძლება
+age_num=56 #space_ვერ გამოვიყენებთ და ვერც ამას =>
+my_fav_mov="blood and bone" #string ბრჭყალებში"უნდა იყოს და სიმბოლოს გამოყენებაც არ შეიძლება! 
+my_friends_name= "  Nika"    # ' აფოსტროფს ვერ გამოვიყენებთ და = მაგივრად ვერც ამას=>
+
+
+print ("my name is " + name1)
+print ("my age is " + str(age_num))
+print ("my favorite movie is" + my_fav_mov)
+print ("my friend name is"  +  my_friends_name)
+
+
+first_name="gela"
+last_name="mchedlishvili"
+full_name=first_name + last_name
+print(full_name)
