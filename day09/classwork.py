1831270e91408009f70de418a0c01e22409e7ecd
2pac-icecyb <gela6ix9ine@gmail.com>
2025-09-07 18:50:13 +0400

    commit

---
+++ day09/classwork.py
@@ -0,0 +1,44 @@
+
+#1
+
+#logical operators არის and და or and დროს აბრუნებს trueს მაშინ როცა ორივე პირობა სწორია 
+#or დროს აბრუნებს true ს როცა ერთი მაინც სწორია
+
+#2
+name = input("Enter your name: ")
+
+name = "ixvi"
+
+#3
+number = int(input("enter your number"))
+print(number>50)
+
+
+#4
+
+
+my_fav = "lego"
+
+
+toy = input("Enter your favorite toy: ")
+
+
+#5
+#true ს გამოიტანს
+
+#6
+print(10 > 5) 
+
+print(3 < 7)    
+
+print(4 == 4)   
+
+print(6 >= 6) 
+
+#7
+
+
+print(True and False)  
+
+
+print(True or False)   
